cbb9050c801d55245ba7e8b795e0447e856c0e04
Jim Myhrberg <contact@jimeh.me>
2010-11-28 11:51:50 +0000

    fixed a typo

--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Redistat::Collection do
   
-  it "should should initialize properly" do
+  it "should initialize properly" do
     options = {:from => "from", :till => "till", :depth => "depth"}
     result = Redistat::Collection.new(options)
     result.from.should == options[:from]
