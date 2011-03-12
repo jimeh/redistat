43fc8bc2dda8932b76546b8dd75d4b9485593870
Jim Myhrberg <contact@jimeh.me>
2011-03-12 22:21:15 +0000

    updated Hash extensions and specs

--- lib/redistat/core_ext/hash.rb
+++ lib/redistat/core_ext/hash.rb
@@ -1,21 +1,23 @@
 class Hash
   
   def merge_and_incr(hash)
+    self.clone.merge_and_incr!(hash)
+  end
+  
+  def merge_and_incr!(hash)
     raise ArgumentError unless hash.is_a?(Hash)
     hash.each do |key, value|
-      if value.is_a?(Numeric)
-        self.set_or_incr(key, value)
-      else
-        self[key] = value
-      end
+      self[key] = value unless self.set_or_incr(key, value)
     end
+    self
   end
   
   def set_or_incr(key, value)
-    return self unless value.is_a?(Numeric)
+    return false unless value.is_a?(Numeric)
     self[key] = 0 unless self.has_key?(key)
-    self[key] += value if self[key].is_a?(Numeric)
-    self
+    return false unless self[key].is_a?(Numeric)
+    self[key] += value
+    true
   end
   
 end
